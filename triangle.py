33ad6825ed8b51e9323c3aca02fbf37a976d22ca
Iris Bore <irissb@uio.no>
2020-10-30 12:09:36 +0100

    colorplotting

--- triangle.py
+++ triangle.py
@@ -2,13 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 corners = np.array([(0, 0), (1, 0), (0.5, np.sqrt(3 / 4))])
-
 plt.scatter(*zip(*corners))
 # plt.show()
 # plt.figure()
-
-weights = np.zeros(3)
-points = []
 
 
 def get_random_point():
@@ -22,6 +18,7 @@ def get_random_point():
 
         ----------
     """
+    weights = np.zeros(3)
     weights = np.array([np.random.random(), np.random.random(), np.random.random()])
     weights = weights / np.sum(weights)
     return weights[1] * corners[1] + weights[2] * corners[2]
@@ -31,10 +28,14 @@ def draw_points():
     """
     Docstring
     """
+    points = []
     for _ in range(1000):
         points.append(get_random_point())
     plt.scatter(*zip(*points))
     plt.show()
+
+
+# draw_points()
 
 
 def iterate_corners():
@@ -42,6 +43,7 @@ def iterate_corners():
     Docstring
     """
     points = []
+    colors = np.zeros(10000)
     x_i = get_random_point()
     random_corner = np.random.randint(0, 3)
 
@@ -53,10 +55,32 @@ def iterate_corners():
         random_corner = np.random.randint(0, 3)
         x_i = (x_i + corners[random_corner]) / 2
         points.append(x_i)
+        colors[i] = random_corner
 
+    return points, colors
+
+
+def plot_points(points):
     plt.scatter(*zip(*points), s=0.1, marker=".")
     plt.axis("equal")
     plt.show()
 
 
-iterate_corners()
+def alt_iterate():
+    points, colors = iterate_corners()
+    n = len(colors)
+    # red = X[colors == 0]
+    # green = X[colors == 1]
+    # blue = X[colors == 2]
+    color = "b"
+    for i in range(n):
+        if colors[i] == 0:
+            plt.scatter(points[i][0], points[i][1], color="r")
+        elif colors[i] == 1:
+            plt.scatter(points[i][0], points[i][1], color="g")
+        else:
+            plt.scatter(points[i][0], points[i][1], color="b")
+    plt.show()
+
+
+alt_iterate()
