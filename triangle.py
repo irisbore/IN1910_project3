7bb777e4cdd058a25b00f724694499d7b7138c35
Iris Bore <irissb@uio.no>
2020-10-30 11:27:39 +0100

    plot details

--- triangle.py
+++ triangle.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 corners = np.array([(0, 0), (1, 0), (0.5, np.sqrt(3 / 4))])
+
 plt.scatter(*zip(*corners))
 # plt.show()
 # plt.figure()
@@ -11,15 +12,25 @@ points = []
 
 
 def get_random_point():
+    """
+    Assigns each weight a random value between 0 and 1.
+    Scales the list of weights so that the sum is 1.
+    Returns a point inside the triangle expressed as a linear combination
+
+    weights: array_like, (float, float, float, float)
+        Output
+
+        ----------
+    """
     weights = np.array([np.random.random(), np.random.random(), np.random.random()])
-    # weights[0] = np.random.random()
-    # weights[1] = np.random.random()
-    # weights[2] = np.random.random()
     weights = weights / np.sum(weights)
     return weights[1] * corners[1] + weights[2] * corners[2]
 
 
-def generate_points():
+def draw_points():
+    """
+    Docstring
+    """
     for _ in range(1000):
         points.append(get_random_point())
     plt.scatter(*zip(*points))
@@ -27,6 +38,9 @@ def generate_points():
 
 
 def iterate_corners():
+    """
+    Docstring
+    """
     points = []
     x_i = get_random_point()
     random_corner = np.random.randint(0, 3)
@@ -40,7 +54,8 @@ def iterate_corners():
         x_i = (x_i + corners[random_corner]) / 2
         points.append(x_i)
 
-    plt.scatter(*zip(*points))
+    plt.scatter(*zip(*points), s=0.1, marker=".")
+    plt.axis("equal")
     plt.show()
 
 
