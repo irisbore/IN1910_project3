f337325d1a4aab829ca14b067faf2db8d7b15117
Iris Bore <irissb@uio.no>
2020-11-06 11:42:51 +0100

    finsihed fern, see figures folder

--- fern.py
+++ fern.py
@@ -1,5 +1,5 @@
 import numpy as np
-import sympy as sp
+import matplotlib.pyplot as plt
 
 
 class AffineTransform:
@@ -12,21 +12,44 @@ class AffineTransform:
         self.f = f
 
     def __call__(self, x, y):
-        print("in call")
         M = np.matrix([[self.a, self.b], [self.c, self.d]])
-        X = np.matrix([x, y]).T
+        X = np.matrix([[x, y]]).T
         c = np.matrix([self.e, self.f]).T
-        return M @ X + c
+        a = M @ X + c
+        a = [float(a[0][0]), float(a[1][0])]
+        return a
 
-    def cumulative(self):
-        functions = [f1,f2,f3,f4]
-        probs = [0.01, 0.85, 0.07, 0.07]
-        probs = probs / sum(probs)
-        r = np.random.random()
-        for j, p in enumerate(probs):
-            if r < p:
-        return functions[j]
 
+def scale(probs):
+    assert sum(probs) == 1, "sum of input must be 1"
+    for i in range(1, len(probs)):
+        probs[i] = probs[i] + probs[i - 1]
+    return probs
+
+
+def cumulative(f1, f2, f3, f4):
+    probs = scale([0.01, 0.85, 0.07, 0.07])
+    functions = [f1, f2, f3, f4]
+    r = np.random.random()
+    for j, p in enumerate(probs):
+        if r < p:
+            return functions[j]
+
+
+def iterate(f1, f2, f3, f4):
+    x0 = [0, 0]
+    x = [x0]
+    for i in range(50000):
+        f = cumulative(f1, f2, f3, f4)
+        x.append(f(x[-1][0], x[-1][1]))
+    return x
+
+
+def plot(f1, f2, f3, f4):
+    x = iterate(f1, f2, f3, f4)
+    plt.scatter(*zip(*x), c="forestgreen", s=0.1)
+    plt.axis("equal")
+    plt.savefig("barnsley_fern.png")
 
 
 if __name__ == "__main__":
@@ -34,3 +57,4 @@ if __name__ == "__main__":
     f2 = AffineTransform(a=0.85, b=0.04, c=-0.04, d=0.85, f=1.60)
     f3 = AffineTransform(a=0.2, b=-0.26, c=0.23, d=0.22, f=1.60)
     f4 = AffineTransform(a=-0.15, b=0.28, c=0.26, d=0.24, f=0.44)
+    plot(f1, f2, f3, f4)
