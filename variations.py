0aaaa942128a6d977a16e252d822768af7269e89
Jonatan <jonatankwins@gmail.com>
2020-11-06 11:52:18 +0100

    merge

--- variations.py
+++ variations.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import triangle
 
 
 class Variations:
@@ -6,6 +8,14 @@ class Variations:
         self.x = x
         self.y = y
         self.name = name
+        self._func = getattr(Variations, name)
+
+    def transform(self):
+        self.x, self.y = self._func(self.x, self.y)
+        return self.x, self.y
+
+    def plot(self, cmap="jet"):
+        plt.scatter(self.x, self.y, cmap=cmap)
 
     @staticmethod
     def linear(x, y):
@@ -42,9 +52,65 @@ class Variations:
         return 2 / (r + 1) * (x, y)
 
     @staticmethod
-    def tangen(x, y):
+    def tangent(x, y):
         return (np.sin(x) / np.cos(y), np.tan(y))
 
     @staticmethod
     def cross(x, y):
         return np.sqrt(1 / (x ** 2 - y ** 2) ** 2) * (x, y)
+
+
+def plot_2_transformations_triangle():
+    points, _ = triangle.iterate_corners()
+    swirl = Variations(points[:, 0], points[:, 1], "swirl")
+    swirl.transform()
+
+    eyefish = Variations(points[:, 0], points[:, 1], "eyefish")
+    eyefish.transform()
+
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    fig.suptitle("two transformations")
+
+    ax1.scatter(swirl.x, swirl.y, c="orange")
+    ax1.set_ylabel("swirl")
+
+    ax2.scatter(eyefish.x, eyefish.y, c="purple")
+    ax2.set_ylabel("eyefish")
+
+
+def transform_grid():
+    N = 80
+    grid_values = np.linspace(-1, 1, N)
+    x, y = np.meshgrid(grid_values, grid_values)
+    x_values = x.flatten()
+    y_values = y.flatten()
+
+    transformations = ["linear", "handkerchief", "swirl", "disc"]
+    variations = [
+        Variations(x_values, y_values, version) for version in transformations
+    ]
+
+    fig, axs = plt.subplots(2, 2, figsize=(9, 9))
+    for i, (ax, variation) in enumerate(zip(axs.flatten(), variations)):
+
+        u, v = variation.transform()
+
+        ax.plot(
+            u,
+            -v,
+            markersize=1,
+            marker=".",
+            linestyle="",
+            color=[np.random.random(), np.random.random(), np.random.random()],
+        )
+        # ax.scatter(u, -v, s=0.2, marker=".", color="black")
+        ax.set_title(variation.name)
+        ax.axis("off")
+
+    # fig.savefig("figures/variations_4b.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot_2_transformations_triangle()
+    transform_grid()
